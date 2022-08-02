b3cd212832509ed71cd975e0faa1e0aef85b69d1
Ankit Raj <ankittrajj1996@gmail.com>
2022-08-02 12:56:55 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,14 +4,46 @@ window = Tk()
 
 window.title('python')
 
+
+# create the function.
+def click():
+    enter_text = str(textentry.get())
+    output.delete(0.0,END)
+
+
+#     exceptional handling
+    try:
+        define = mydict[enter_text]
+    except:
+        define = 'not available'
+    output.insert(END,define)
+
 # textbox
-textbox = Entry(window,width = 50,bg = 'red').grid(row = 0 , column=0)
+textentry = Entry(window,width = 50,bg = 'red')
+textentry.grid(row = 0 , column=0)
 
 # label
 Label(window,text = "I am GUI",width = 50,bg = 'white',fg='black').grid(row =1,column=1)
 
-Button(window,text = 'SUBMIT',width = 20, bg = 'red',fg = 'black').grid(row=1,column=0)
+Button(window,text = 'SUBMIT',command=click,width = 20, bg = 'red',fg = 'black').grid(row=1,column=0)
 
+
+# label
+Label(window,text = 'Defenation', bg ='red',fg='black',font = 'none 12 bold').grid(row=1,column =2)
+
+# o/p text box
+
+output = Text(window,width = 40,height = 10,bg = 'white')
+output.grid(row=0,column=3)
+
+
+# create the data
+
+
+mydict = {
+    'animal':['dogs','cats'],
+    'python': 'a useful prog. language'
+}
 
 
 
